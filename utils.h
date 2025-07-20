0ddf50395f9f36d03fe94c4c63eaab58cf981c28
Egor Sholukh <egor.bulochka@gmail.com>
2025-07-21 00:36:37 +0300

    Generated docstring

--- utils.h
+++ utils.h
@@ -2,12 +2,23 @@
 
 #include <chrono>
 
-class Utils {
+class Utils
+{
 
 public:
-
+    /**
+     * @brief Measures the execution time of a function.
+     *
+     * This method accepts a callable (function or lambda) object and returns the duration in seconds.
+     * It measures the start time before calling the function, and end time after it's done, then calculates the difference.
+     *
+     * @tparam F The type of the function/lambda to be measured.
+     * @param func The callable (function or lambda) object to measure.
+     * @return float The duration in seconds.
+     */
     template <typename F>
-    static float measure(F func) {
+    static float measure(F func)
+    {
         auto start_time = std::chrono::high_resolution_clock::now();
         func();
         auto end_time = std::chrono::high_resolution_clock::now();
@@ -16,8 +27,22 @@ public:
         return elapsed.count();
     }
 
-    template<typename T>
-    static T* create_array(const size_t size, const size_t group_size = 1, const T val = T(0)) {
+    /**
+     * @brief Creates a new array of the given size, filled with zeros or a specified value.
+     *
+     * This function creates an array of the given 'size' and fills it with either zeros (default) or a user-specified value.
+     * If the 'group_size' argument is provided, the array will be padded at the end to ensure that it's a multiple of 'group_size'.
+     * The function returns a pointer to the newly created array.
+     *
+     * @tparam T The type of the elements in the array.
+     * @param size The desired size of the new array.
+     * @param group_size The size of the groups for padding purposes (default is 1).
+     * @param val The value to fill the array with (default is 0).
+     * @return T* A pointer to the newly created and filled array.
+     */
+    template <typename T>
+    static T *create_array(const size_t size, const size_t group_size = 1, const T val = T(0))
+    {
         const int groups_count = (size + group_size - 1) / group_size;
         const int global_size = group_size * groups_count;
 
@@ -29,19 +54,43 @@ public:
         return data;
     }
 
-    template<typename T>
-    static void randomize_array(T* data, const size_t size) {
+    /**
+     * @brief Randomizes the elements in an array.
+     *
+     * This function takes a pointer to an array and its 'size', then fills the array with random values from a uniform distribution between -1 and 1.
+     *
+     * @tparam T The type of the elements in the array.
+     * @param data A pointer to the array to be randomized.
+     * @param size The size of the array.
+     */
+    template <typename T>
+    static void randomize_array(T *data, const size_t size)
+    {
         std::mt19937 generator(42);
         std::uniform_real_distribution<T> dist(-1, 1);
 
-        for (size_t i = 0; i < size; i++) {
+        for (size_t i = 0; i < size; i++)
+        {
             data[i] *= dist(generator);
         }
     }
 
-    template<typename T>
-    static void fill_array(T* data, const size_t size, const T val = T(0)) {
-        for (size_t i = 0; i < size; i++) {
+    /**
+     * @brief Fills an array with the specified value.
+     *
+     * This function takes a pointer to an array, its 'size', and a value to fill the array with.
+     * It fills the array with the provided value.
+     *
+     * @tparam T The type of the elements in the array.
+     * @param data A pointer to the array to be filled.
+     * @param size The size of the array.
+     * @param val The value to fill the array with (default is 0).
+     */
+    template <typename T>
+    static void fill_array(T *data, const size_t size, const T val = T(0))
+    {
+        for (size_t i = 0; i < size; i++)
+        {
             data[i] = val;
         }
     }
--- vector.h
+++ vector.h
@@ -2,59 +2,145 @@
 
 #include <iostream>
 
-
+/**
+ * @class Vector
+ *
+ * @brief This class represents a vector with data of type T and size _size. It has methods for sum, addition, multiplication, dot product and printing the vector.
+ *
+ * The template parameter T is used to specify the type of the elements in the vector. The size of the vector is stored in _size.
+ *
+ * @tparam T - the type of data stored in the vector.
+ */
 template <typename T>
-class Vector {
+class Vector
+{
 
 public:
-    Vector(T *data, size_t size): 
-        _data(data), _size(size) {}
+    /**
+     * @brief Constructor for the Vector class.
+     *
+     * It takes two parameters, a pointer to an array of elements of type T and size_t that specifies its size.
+     *
+     * @param data - a pointer to an array of elements of type T.
+     * @param size - the size of the vector.
+     */
+    Vector(T *data, size_t size) : _data(data), _size(size) {}
 
-    T* data() const { return _data; }
+    /**
+     * @brief Accessor method for the private member variable '_data'.
+     *
+     * @return T* - a pointer to the vector's elements.
+     */
+    T *data() const { return _data; }
 
+    /**
+     * @brief Accessor method for the private member variable '_size'.
+     *
+     * @return size_t - the size of the vector.
+     */
     size_t size() const { return _size; }
 
-    virtual T sum() const {
+    /**
+     * @brief Returns the sum of all elements in the vector.
+     *
+     * This method iterates over all the elements in the vector and returns their sum.
+     *
+     * @return T - the sum of all elements in the vector.
+     */
+    virtual T sum() const
+    {
         T val = T(0);
-        for (size_t i = 0; i < _size; i++) {
+        for (size_t i = 0; i < _size; i++)
+        {
             val += _data[i];
         }
         return val;
     }
 
-    virtual void add(const Vector<T> &o) {
-        for (size_t i = 0; i < _size; i++) {
+    /**
+     * @brief Adds another vector to this one element-wise.
+     *
+     * This method iterates over all the elements in both vectors and adds corresponding elements from the second vector to the first.
+     *
+     * @param o - the Vector to add to this one.
+     */
+    virtual void add(const Vector<T> &o)
+    {
+        for (size_t i = 0; i < _size; i++)
+        {
             _data[i] += o._data[i];
         }
     }
 
-    virtual void mul(const Vector<T> &o) {
-        for (size_t i = 0; i < _size; i++) {
+    /**
+     * @brief Multiplies this vector with another one element-wise.
+     *
+     * This method iterates over all the elements in both vectors and multiplies corresponding elements from the second vector to the first.
+     *
+     * @param o - the Vector to multiply this one by.
+     */
+    virtual void mul(const Vector<T> &o)
+    {
+        for (size_t i = 0; i < _size; i++)
+        {
             _data[i] *= o._data[i];
         }
     }
 
-    virtual T dot(const Vector<T> &o) const {
+    /**
+     * @brief Computes the dot product of this vector with another one.
+     *
+     * This method iterates over all the elements in both vectors and computes their dot product.
+     *
+     * @param o - the Vector to compute the dot product with.
+     *
+     * @return T - the result of the dot product.
+     */
+    virtual T dot(const Vector<T> &o) const
+    {
         T val = T(0);
 
-        for (size_t i = 0; i < _size; i++) {
+        for (size_t i = 0; i < _size; i++)
+        {
             val += _data[i] * o._data[i];
         }
+
         return val;
     }
 
-    virtual void print() const {
+    /**
+     * @brief Prints the vector to the standard output.
+     *
+     * This method prints all elements of the vector to the standard output, separated by commas and enclosed in square brackets.
+     */
+    virtual void print() const
+    {
         std::cout << "[";
-        for (size_t i = 0; i < _size; i++) {
+
+        for (size_t i = 0; i < _size; i++)
+        {
             std::cout << _data[i];
-            if (i != _size - 1) {
+
+            if (i != _size - 1)
+            {
                 std::cout << ",";
             }
         }
+
         std::cout << "]";
     }
 
-    size_t size_mb() { return (sizeof(T) * _size) / (1024 * 1024); }
+    /**
+     * @brief Returns the size of this vector in megabytes.
+     *
+     * This method returns the size of this vector in megabytes, calculated by dividing its total size (in bytes) by 1024*1024.
+     *
+     * @return size_t - the size of this vector in megabytes.
+     */
+    virtual size_t size_mb() const
+    {
+        return (sizeof(T) * _size) / (1024 * 1024);
+    }
 
 protected:
     T *_data;
