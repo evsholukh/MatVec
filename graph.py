2d62563a439dae3baba74b3298529820d2869c93
evsholukh <egor.bulochka@gmail.com>
2025-08-09 11:17:05 +0300

    Исправление

--- graph.py
+++ graph.py
@@ -13,12 +13,15 @@ num_elemets = [u*v for u, v in zip(block_sizes, blocks_count)]
 
 for i in blocks_count:
     for j in block_sizes:
-        args = ["matmul.exe", "-n", str(i), "-b", str(j)]
-        print(args, file=sys.stderr)
+        try:
+            args = ["matmul.exe", "-n", str(i), "-b", str(j)]
+            print(args, file=sys.stderr)
 
-        output = subprocess.check_output(args, timeout=120.0, start_new_session=True)
-        data = json.loads(str(output, encoding="utf-8"))
-        total += data
-
+            output = subprocess.check_output(args, timeout=120.0, start_new_session=True)
+            data = json.loads(str(output, encoding="utf-8"))
+            total += data
+        except Exception as e:
+            print(str(e), file=sys.stderr)
+            continue
 
 json.dump(total, sys.stdout, indent=2)
