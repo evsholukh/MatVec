646cd855459307d9f50e64d4da8a59524820662f
Egor Sholukh <egor.bulochka@gmail.com>
2025-08-14 19:42:38 +0300

    Modified scripts

--- graph.py
+++ graph.py
@@ -5,9 +5,9 @@ import tqdm
 
 total = []
 
-block_sizes = [2**i for i in range(11)]
-grid_sizes = [2**i for i in range(11)]
-num_elemets = [10**8] #[10**i for i in range(7)]
+block_sizes = [2**i for i in range(4, 11)]
+grid_sizes = [2**i for i in range(4, 11)]
+num_elemets = [10**8]
 
 
 pg = tqdm.tqdm(total=len(block_sizes)*len(grid_sizes)*len(num_elemets), file=sys.stderr)
@@ -16,7 +16,7 @@ for gs in grid_sizes:
     for bs in block_sizes:
         for sz in num_elemets:
             try:
-                args = ["matmul_cuda.exe", "-s", str(sz), "-b", str(bs), "-g", str(gs)]
+                args = ["matmul.exe", "-s", str(sz), "-b", str(bs), "-g", str(gs)]
                 print(args, file=sys.stderr)
 
                 output = subprocess.check_output(args, timeout=120.0, start_new_session=True)
