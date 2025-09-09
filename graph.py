9d743d31abe89c71da3ed98c57945f81fc770517
Egor Sholukh <egor.bulochka@gmail.com>
2025-09-09 21:00:45 +0300

    New graph

--- graph.py
+++ graph.py
@@ -1,22 +1,26 @@
 import subprocess
 import json
+import sys
+import tqdm
 
-
-N = 8*(10**8)
+T = 1_000_000
+N = [i for i in range(T, T*100, T)] # 8*(10**8)
 progs = ["vectors.exe", "vectors_cuda.exe"]
 
 total = []
 for exe in progs:
-    try:
-        args = [exe, "-n", str(N), "-b", str(1024), "-g", str(1024*1024)]
-        print(*args)
-        output = subprocess.check_output(args, timeout=1200.0)
-        data = json.loads(str(output, encoding="utf-8"))
-        total += data
-    except KeyboardInterrupt:
-        break
-    except Exception as e:
-        print(str(e))
-        continue
+    for n in tqdm.tqdm(N):
+        try:
+            args = [exe, "-n", str(n), "-b", str(1024), "-g", str(1024*1024)]
+            # print(*args)
+            output = subprocess.check_output(args, timeout=1200.0)
+            data = json.loads(str(output, encoding="utf-8"))
+            # json.dump(data, sys.stderr, indent=2)
+            total += data
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(str(e))
+            break
 
 json.dump(total, open("metrics.json", "w"), indent=2)
